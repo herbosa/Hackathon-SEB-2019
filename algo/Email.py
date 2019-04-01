c83d5ed0a4fb842ee632f8a498732e794aff7af9
myos <myosxcorp@gmail.com>
2019-04-01 11:58:11 +0200

    Email.py

--- algo/Email.py
+++ algo/Email.py
@@ -12,6 +12,11 @@ class Email:
     CONTENT_TRANSFER_ENCODING = 7
     X_FROM = 8
     X_TO = 9
+    X_CC = 10
+    X_BCC = 11
+    X_FOLDER = 12
+    X_ORIGIN = 13
+    X_FILENAME = 14
 
     def __init__(self, email_string: str) :
         """Initialize the email from string."""
@@ -30,6 +35,12 @@ class Email:
         self.content_transfer_encoding = lines[self.CONTENT_TRANSFER_ENCODING][27:]
         self.x_from = lines[self.X_FROM][8:]
         self.x_to = lines[self.X_TO][6:]
+        self.xcc = lines[self.X_CC][6:]
+        self.xbcc = lines[self.X_BCC][7:]
+        self.xfolder = lines[self.X_FOLDER][10:]
+        self.xorigin = lines[self.X_ORIGIN][10:]
+        self.xfilename = lines[self.X_FILENAME][13:]
+
 
     def display(self):
         print("MESSAGE ID:", self.message_id)
@@ -42,6 +53,11 @@ class Email:
         print("CONTENT_TRANSFER_ENCODING:", self.content_transfer_encoding)
         print("X_FROM:", self.x_from)
         print("X_TO:", self.x_to)
+        print("X_CC:", self.xcc)
+        print("X_BCC:", self.xbcc)
+        print("X_FOLDER:", self.xfolder)
+        print("X_ORIGIN:", self.xorigin)
+        print("X_FILENAME:", self.xfilename)
 
 if __name__ == '__main__' :
     f = open("../data/email_example")
