91c79781f29d15bdcec70ad8c55f0dd008552906
alex <alexandre.fourcat@epitech.eu>
2019-04-02 10:52:31 +0200

    Virtualenv

---
+++ demo/serveur/django-demo/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alex/Epitech/Hackathon/Hackathon-SEB-2019/demo/serveur/django-demo/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
