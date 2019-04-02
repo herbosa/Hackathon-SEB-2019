c382bf2fe14963860ebbfe4141ca1c5243759fda
alex <alexandre.fourcat@epitech.eu>
2019-04-02 13:40:53 +0200

    Adding last thing.

--- demo/serveur/demo/views.py
+++ demo/serveur/demo/views.py
@@ -2,9 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 
+DATA_JSON = """{
+    "tasks": [
+        { "description":
+            { "name": "Reunion Projet info",
+              "time": 60,
+              "skills": ["Java", "Swift"]
+            },
+          "preferences":
+            { "days": ["Mon", "Tue"],
+              "attendees": ["Antoine.H"]
+            }
+        }
+    ]
+}"""
+
 def distribute_mails(request, content):
     if request.method == 'GET':
-        print(content)
+        print(DATA_JSON)
         return HttpResponse("{\"status\": 200 }", content_type='application/json')
     else:
         return HttpResponse("{\"status\": 400 }", content_type='application/json')
