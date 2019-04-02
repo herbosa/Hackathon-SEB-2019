978856c0a8bec15daa5813c3e36cae020835db59
alex <alexandre.fourcat@epitech.eu>
2019-04-02 14:30:14 +0200

    Views.

--- demo/serveur/demo/views.py
+++ demo/serveur/demo/views.py
@@ -5,7 +5,7 @@ import requests
 DATA_JSON = """{
     "tasks": [
         { "description":
-            { "name": "Reunion Projet info",
+            { "name": "mailbox IOS/Android application",
               "time": 60,
               "skills": ["Java", "Swift"]
             },
@@ -17,9 +17,39 @@ DATA_JSON = """{
     ]
 }"""
 
+"I would like to take an appointement during 1h about the IOS/Android mailbox application."
+
+DATA_JSON_2 = """{
+    "tasks": [
+        { "description":
+            { "name": "for the April financial meeting.",
+              "time": 30,
+              "skills": ["Comptat"]
+            },
+          "preferences":
+            { "days": ["Mon", "Tue"],
+              attendees": [Quentin.H"]
+            }
+        }
+    ]
+}"""
+
+"I need someone half an hour for the April financial meeting."
+
+FIRST = True
+
 def distribute_mails(request, content):
+    global FIRST
+
     if request.method == 'GET':
-        print(DATA_JSON)
+        print(content)
+        if FIRST:
+            FIRST = False
+            # Send DATA_JSON
+            print(DATA_JSON)
+        else:
+            print(DATA_JSON_2)
+            # Send DATA_JSON_2
         return HttpResponse("{\"status\": 200 }", content_type='application/json')
     else:
         return HttpResponse("{\"status\": 400 }", content_type='application/json')
